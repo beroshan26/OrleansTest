ccb09534417ce05303cd9d1355ff475c179fd869
beroshan26 <32231715+beroshan26@users.noreply.github.com>
2018-11-05 16:44:48 +1100

    added orleans test code

---
+++ OrleansTest/Client/Program.cs
@@ -0,0 +1,43 @@
+﻿using Interfaces;
+using Orleans;
+using Orleans.Runtime;
+using Orleans.Runtime.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+
+            var config = ClientConfiguration.LoadFromFile("ClientConfiguration.xml");
+
+            while (true)
+            {
+                try
+                {
+                    GrainClient.Initialize(config);
+                    Console.WriteLine("Connected to silo!");
+
+                    var friend = GrainClient.GrainFactory.GetGrain<IPerson>("Joe");
+                    var result = friend.SayHelloAsync("Hello!");
+
+                    Console.ReadLine();
+                    break;
+                }
+                catch (SiloUnavailableException)
+                {
+                    Console.WriteLine("Silo not available! Retrying in 3 seconds.");
+                    Thread.Sleep(3000);
+                }
+            }
+        }
+    }
+}
---
+++ OrleansTest/Grains/PersonGrain.cs
@@ -0,0 +1,21 @@
+﻿using Interfaces;
+using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grains
+{
+    public class PersonGrain : Grain, IPerson
+    {
+        public Task SayHelloAsync(string message)
+        {
+            string name = this.GetPrimaryKeyString();
+            Console.WriteLine($"{name} says: {message}");
+
+            return TaskDone.Done;
+        }
+    }
+}
